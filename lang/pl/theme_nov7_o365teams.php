accf9b266585efbc6e5c86692711c590d71941a9
November7 <m.kowalski.nov7@outlook.com>
2020-10-04 22:01:33 +0200

    Init theme

---
+++ lang/pl/theme_nov7_o365teams.php
@@ -0,0 +1,47 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Boost o365teams.
+ *
+ * @package    theme_boost_o365teams
+ * @copyright  2018 Enovation Solutions
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['choosereadme'] = 'Nov7 o365teams is a modern highly-customisable theme. This theme is intended to be used directly, or as a parent theme when creating new themes utilising Bootstrap 4.';
+$string['pluginname'] = 'Nov7 Office365 Teams';
+$string['region-side-pre'] = 'Right';
+
+$string['placementfrontpage'] = 'Strona główna';
+$string['editcourse'] = 'Edytuj kurs';
+
+$string['share_feedback'] = 'Udostępnij informację zwrotną';
+$string['feedback'] = 'Informacja zwrotna';
+$string['edit_course'] = 'Edytuj kurs w oknie przeglądarki';
+$string['open_course'] = 'Otwórz kurs w oknie przeglądarki';
+
+$string['navigatereturn'] = 'Wróć do menu quizu';
+
+$string['configtitle'] = 'Nov7 Office365 Teams';
+$string['generalsettings'] = 'Ustawienia podstawowe';
+$string['showteamfeedbacklink'] = 'Pokazuj link informacji zwrotnej';
+$string['showteamfeedbacklink_desc'] = 'Jeśli ta opcja jest włączona, po otwarciu karty Moodle w aplikacji Teams zostanie wyświetlone łącze umożliwiające wystawienie opinii na temat integracji Moodle Office 365.';
+$string['advancedsettings'] = 'Ustawienia zaawansowane';
+$string['footer_stamp_title'] = "Stempel zakładek w stopce";
+$string['footer_stamp_desc'] = "Wyświetla obraz lub logo w prawym dolnym rogu zakładek.<br/>(Max wysokość: 60px)";
